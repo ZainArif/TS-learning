7e44d94e1f3539a658d6089147c8a02cb6c04b8e
Zain Arif <zainarif14197@gmail.com>
2022-08-18 23:03:13 +0500

    void, any, never, unknown types & type assertion

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,19 @@
+/*
+node commands
+
+node -v                                 --- to check version of node
+
+npm install -g typescript               --- to install typescript
+
+tsc -v                                  --- to check version of typescript
+
+tsc main.ts                             --- to transpiled into js
+
+tsc main.ts -w                          --- compilation in watch mode
+
+tsc -w                                  --- watch mode for whole project using ts config file
+
+*/
 var a = "1";
 console.log(a);
 var hello = "world";
@@ -23,3 +39,26 @@ console.log(user1.getMessage());
 var page = "1";
 var user3 = null; // | = union operator for mulitple type support
 var popularTags = ["car", "tea"];
+var printName = function (firstName, lastName) {
+    console.log(firstName + " " + lastName);
+};
+var objA = undefined;
+objA = null;
+//objA = "object";     type "void" can be null or undefined
+var objB = "abc";
+objB = 2;
+objB = true;
+//console.log(objB.getName());     type "any" turns off typescript checks
+var callNever = function () {
+    //console.log('type never');
+    throw 'never';
+};
+var vAny = 10;
+var vUnknown = 10; // type "unknown" means no type
+var s1 = vAny;
+//let s2: string = vUnknown;   // ts dont konw the type of variable "vUnknown" 
+//console.log(vUnknown.foo());   ts checks that foo() is not property
+var s3 = vUnknown; // "as" operator makes type assertion
+var pageNumber = "1";
+//let numericPageNumber: number = pageNumber as number;   // cant convert type directy to another type, first have to convert into unknown type
+var numericPageNumber = pageNumber;
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,22 @@
+/*
+node commands
+
+node -v                                 --- to check version of node
+
+npm install -g typescript               --- to install typescript
+
+tsc -v                                  --- to check version of typescript
+
+tsc main.ts                             --- to transpiled into js
+
+tsc main.ts -w                          --- compilation in watch mode
+
+tsc -w                                  --- watch mode for whole project using ts config file
+
+*/
+
+
+
 const a = "1";
 
 console.log(a);
@@ -45,3 +64,45 @@ type popularTag = string;    // type alias
 type mayBePopularTag = popularTag | null;  // union + type alias
 
 const popularTags: mayBePopularTag[] = ["car","tea"];
+
+
+const printName = (firstName: string, lastName: string): void =>{
+    console.log(firstName + " " + lastName);
+}
+
+let objA: void = undefined ;
+
+objA = null;
+
+//objA = "object";     type "void" can be null or undefined
+
+
+let objB: any = "abc";
+
+objB = 2;
+
+objB = true;
+
+//console.log(objB.getName());     type "any" turns off typescript checks
+
+
+const callNever = (): never =>{    // type "never" means nothing to do
+    //console.log('type never');
+    throw 'never';
+}
+
+
+let vAny: any = 10;
+let vUnknown: unknown = 10;   // type "unknown" means no type
+
+let s1: string = vAny;
+//let s2: string = vUnknown;   // ts dont konw the type of variable "vUnknown" 
+
+//console.log(vUnknown.foo());   ts checks that foo() is not property
+
+
+let s3: string = vUnknown as string;   // "as" operator makes type assertion
+
+let pageNumber: string = "1";
+//let numericPageNumber: number = pageNumber as number;   // cant convert type directy to another type, first have to convert into unknown type
+let numericPageNumber: number = (pageNumber as unknown) as number;
